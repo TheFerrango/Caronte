1066eade3103bbd79f209beee303694404645437
unknown <ll@asg-pc100.dsaidea.local>
2015-07-24 11:43:23 +0200

    stile della pagina master situation preparate funzioni mock per recuperare i dati di percorso aggiunte iconcine furgone sulla mappa

--- LogPosizioni/MainPage.xaml.cs
+++ LogPosizioni/MainPage.xaml.cs
@@ -21,8 +21,8 @@ namespace LogPosizioni
 
 		private void btnTrack_Tap(object sender, System.Windows.Input.GestureEventArgs e)
 		{
-			
-			if(isTracking)
+
+			if (isTracking)
 			{
 				btnTrack.Content = "Inizia tracciamento";
 				gcw.Stop();
@@ -44,23 +44,32 @@ namespace LogPosizioni
 				gcw.Stop();
 			}
 
-			string toSend = ReadFile();
+			try
+			{
+				string toSend = ReadFile();
 			
-			if (MessageBox.Show("Cancellare il file delle posizioni inviate?", "Domanda", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
-				DeleteFile();
+				if (MessageBox.Show("Cancellare il file delle posizioni inviate?", "Domanda", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+					DeleteFile();
 
-			EmailComposeTask ect = new EmailComposeTask();
-			ect.To = "theferrango@outlook.com";
-			ect.Subject = "GPS coords foar Caronte";
-			ect.Body = "[" + toSend + "]";
-			ect.Show();
+				EmailComposeTask ect = new EmailComposeTask();
+				ect.To = "theferrango@outlook.com";
+				ect.Subject = "GPS coords foar Caronte";
+				ect.Body = "[" + toSend + "]";
+				ect.Show();
+			}
+			catch 
+			{
+				MessageBox.Show("Impossibile caricare le posizioni, file non presente");
+			}
+
+			
 		}
 
 		private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
 		{
 			gc = new GeoCoordinate();
 			gcw = new GeoCoordinateWatcher();
-			gcw.MovementThreshold = 10;
+			gcw.MovementThreshold = 1;
 			gcw.PositionChanged += gcw_PositionChanged;
 		}
 
@@ -70,13 +79,14 @@ namespace LogPosizioni
 			lon.Text = e.Position.Location.Longitude.ToString();
 			alt.Text = e.Position.Location.Altitude.ToString();
 			pre.Text = e.Position.Location.HorizontalAccuracy.ToString();
-			SaveFile(string.Format("{{ 'IDPosizione' : 0, 'FKIDViaggio' : 1, 'Data': \"{0}\", 'Latitudine': {1}, 'Longitudine': {2}, 'Precisione:' {3}}},",e.Position.Timestamp.ToString(), e.Position.Location.Latitude, e.Position.Location.Longitude, e.Position.Location.HorizontalAccuracy));
+			SaveFile(string.Format("{{ 'IDPosizione' : 0, 'FKIDViaggio' : 1, 'Data': \"{0}\", 'Latitudine': {1}, 'Longitudine': {2}, 'Precisione:' {3}}},", e.Position.Timestamp.ToString(), e.Position.Location.Latitude, e.Position.Location.Longitude, e.Position.Location.HorizontalAccuracy));
 		}
 
 		public void SaveFile(string data)
 		{
 			using (IsolatedStorageFile storage = IsolatedStorageFile.GetUserStoreForApplication())
 			{
+				//storage.get
 				using (var fileStream = new IsolatedStorageFileStream("posizioni.gps", FileMode.Append, FileAccess.Write, storage))
 				{
 					using (StreamWriter writer = new StreamWriter(fileStream))
@@ -108,7 +118,7 @@ namespace LogPosizioni
 		}
 
 		public void DeleteFile()
-		{			
+		{
 			using (IsolatedStorageFile storage = IsolatedStorageFile.GetUserStoreForApplication())
 			{
 				storage.DeleteFile("posizioni.gps");
